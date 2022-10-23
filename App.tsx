f57c600df8a00ca3a5c121f0dec3604c972760d7
michelangelo <michelangelocali@hotmail.com>
2022-10-23 00:24:37 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
+
+import theme from './src/global/styles/theme'
+import { Dashboard } from './src/pages/Dashboard/Dashboard';
+
+export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
+}
