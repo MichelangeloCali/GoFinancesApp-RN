86f2a7a66883d3124fa13cb9a2c5389c98ac213f
michelangelo <michelangelocali@hotmail.com>
2022-10-23 17:23:52 -0300

    icons header added

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons'
 
 export const Container = styled.View`
   flex: 1;
@@ -17,6 +18,10 @@ export const Header = styled.View`
 `
 
 export const UserWrapper = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  
   width: 100%;
   padding: 0 24px;
 `
@@ -47,4 +52,9 @@ export const UserName = styled.Text`
   color: ${({ theme }) => theme.colors.shape};
   font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
+`
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
 `
