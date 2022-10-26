6432707835ddf78f30bf6d54945cc0c81d335bc3
michelangelo <michelangelocali@hotmail.com>
2022-10-25 23:03:37 -0300

    status-bar adjustment

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -9,7 +10,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
   width: 100%;
   height: ${RFPercentage(42)}px;
@@ -24,6 +25,7 @@ export const UserWrapper = styled.View`
   
   width: 100%;
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
 `
 
 export const UserInfo = styled.View`
@@ -61,6 +63,10 @@ export const Icon = styled(Feather)`
 //acessando propriedades do elemento pelo styled components
 export const HighlightCards = styled.ScrollView.attrs({
   horizontal: true,
-  showsHorizontalScrollIndicator: true,
-  contentContainerStyle: { paddingHorizontal:24 }
-})``
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 }
+})`
+  width: 100%;
+  position: absolute;
+  margin-top: ${RFPercentage(20)}px;
+`;
