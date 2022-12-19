b6a6a972a8d22fb1f01a89e5fbe3f27394796536
michelangelo <michelangelocali@hotmail.com>
2022-12-18 22:09:19 -0300

    react tab navigation

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -11,6 +11,7 @@ import {
   User,
   UserGreeting,
   UserName,
+  LogoutButton,
   Icon,
   HighlightCards,
   Transactions,
@@ -73,7 +74,9 @@ export const Dashboard = () => {
             </User>
           </UserInfo>
 
-          <Icon name="power" />
+          <LogoutButton onPress={() => {}}>
+            <Icon name="power" />
+          </LogoutButton>
         </UserWrapper>
       </Header>
 
